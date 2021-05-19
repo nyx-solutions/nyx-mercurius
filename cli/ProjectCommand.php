c95618aefdcc0e1ade9a679fa7323776847918d9
Jonatas Sas <atendimento@jsas.com.br>
2021-05-18 21:13:46 -0300

    Basic

---
+++ cli/ProjectCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+    namespace nyx\mercurius\wp\cli;
+
+    /**
+     * NYX Mercurius Project Management Command
+     */
+    class ProjectCommand extends MercuriusCommand
+    {
+        /**
+         * @param string
+         */
+        protected string $version = '1.0.0';
+
+        /**
+         * Prints the NYX Mercurius Project Management Command
+         *
+         * @param array $params
+         * @param array $extendedParams
+         */
+        public function version(array $params = [], array $extendedParams = []): void
+        {
+            WP_CLI::success("NYX Mercurius WP-CLI Project Management version {$this->version}.");
+        }
+    }
